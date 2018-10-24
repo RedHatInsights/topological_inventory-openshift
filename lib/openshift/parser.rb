cce4461829b837247a5912259e7c8f932f363a49
Martin Slemr <mslemr@redhat.com>
2018-10-24 13:59:38 +0200

    Rename ServiceParametersSets to ServicePlans
    following core and ingress api

--- lib/openshift/parser.rb
+++ lib/openshift/parser.rb
@@ -22,7 +22,7 @@ module Openshift
     def initialize
       entity_types = [:container_groups, :container_nodes, :container_projects,
                       :container_templates, :service_instances, :service_offerings,
-                      :service_parameters_sets]
+                      :service_plans]
 
       self.collections = entity_types.each_with_object({}).each do |entity_type, collections|
         collections[entity_type] = TopologicalInventory::IngressApi::Client::InventoryCollection.new(:name => entity_type)
