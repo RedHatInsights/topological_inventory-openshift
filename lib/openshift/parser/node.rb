f22883557b7ec6dcb2c246900881d966ab93e871
Adam Grare <agrare@redhat.com>
2018-12-06 11:08:47 -0500

    Fix providerID parsing for AWS

--- lib/openshift/parser/node.rb
+++ lib/openshift/parser/node.rb
@@ -33,7 +33,11 @@ module Openshift
       def vm_cross_link(provider_id)
         return if provider_id.nil?
 
-        lazy_find(:cross_link_vms, {:uid_ems => provider_id})
+        # AWS providerID format aws:///us-west-2b/i-02ca66d00f6485e3e
+        _, instance_uri = provider_id.split("://", 2)
+        uid_ems = instance_uri.split("/").last
+
+        lazy_find(:cross_link_vms, {:uid_ems => uid_ems})
       end
     end
   end
