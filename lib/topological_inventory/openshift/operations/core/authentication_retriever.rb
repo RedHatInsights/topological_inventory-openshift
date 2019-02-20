0129078e4d70f3e0f20daea083689cd5b8804d7b
Adam Grare <agrare@redhat.com>
2019-02-20 08:52:54 -0500

    Temporarily remove authentication retriever
    Disable AuthenticationRetriever until it can be replaced with a
    version using the internal API

--- lib/topological_inventory/openshift/operations/core/authentication_retriever.rb
+++ lib/topological_inventory/openshift/operations/core/authentication_retriever.rb
@@ -10,7 +10,7 @@ module TopologicalInventory
           def process
             # Without using Rails example:
             # https://github.com/ManageIQ/topological_inventory-orchestrator/pull/1/files#diff-4743ee12ee7e621468f2e6590de994efR97
-            Authentication.where(:resource_type => "Endpoint", :resource_id => @endpoint_id).first
+            # Authentication.where(:resource_type => "Endpoint", :resource_id => @endpoint_id).first
           end
         end
       end
