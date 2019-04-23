7308c0d82063ac54248faf6b5013c8b2be61e08f
Erik Clarizio <eclarizio@gmail.com>
2019-04-23 12:54:31 -0700

    Replace :id in completed task context with correct svc_instance id

--- spec/topological_inventory/openshift/operations/processor_spec.rb
+++ spec/topological_inventory/openshift/operations/processor_spec.rb
@@ -57,7 +57,8 @@ RSpec.describe TopologicalInventory::Openshift::Operations::Processor do
               :reason => "ProvisionedSuccessfully"
             )
           ]
-        }
+        },
+        :id       => 123
       )
     end
 
@@ -100,8 +101,8 @@ RSpec.describe TopologicalInventory::Openshift::Operations::Processor do
         :service_instance => {
           :source_id  => source.id,
           :source_ref => service_instance.source_ref,
-          :url        => "#{base_url_path}service_instances/#{service_instance.id}",
-          :id         => service_instance.id
+          :id         => service_instance.id.to_s,
+          :url        => "#{base_url_path}service_instances/#{service_instance.id}"
         }
       }.to_json
 
