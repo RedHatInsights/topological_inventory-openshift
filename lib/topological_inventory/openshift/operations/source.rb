b09d5f4264808ef041ce9caaa0e28f8c07b9c3a5
Alberto Bellotti <abellott@redhat.com>
2019-07-28 12:12:54 -0400

    Added logic to update the Source's availability_status via the api client. Requires updated ruby client from latest sources to know about the new attribute.

--- lib/topological_inventory/openshift/operations/source.rb
+++ lib/topological_inventory/openshift/operations/source.rb
@@ -1,9 +1,32 @@
+require "sources-api-client"
+
 module TopologicalInventory
   module Openshift
     module Operations
       module Source
         def self.availability_check(params)
-          puts "Running: availability_check for Source id: #{params["source_id"]}"
+          source_id = params["source_id"]
+          raise "Missing source_id for the availability_check request" unless source_id
+
+          # Let's skip the request if it's older than a minute ago.
+          if params["timestamp"]
+            return if params["timestamp"] < (Time.now.utc - 1.minute)
+          end
+
+          puts "Running: availability_check for Source id: #{source_id}"
+
+          api_client = SourcesApiClient::DefaultApi.new
+
+          # TODO: Connection check
+
+          source = SourcesApiClient::Source.new
+          source.availability_status = "available"
+
+          begin
+            api_client.update_source(source_id, source)
+          rescue SourcesApiClient::ApiError => e
+            puts "Failed to update Source id:#{source_id} - #{e}"
+          end
         end
       end
     end
