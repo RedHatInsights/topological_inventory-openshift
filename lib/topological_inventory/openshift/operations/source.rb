e614744d900c686d455f9c890416f41e00114cf3
Alberto Bellotti <abellott@redhat.com>
2019-07-29 10:27:54 -0400

    Added a connection check for the default endpoint of a source.

--- lib/topological_inventory/openshift/operations/source.rb
+++ lib/topological_inventory/openshift/operations/source.rb
@@ -4,6 +4,8 @@ module TopologicalInventory
   module Openshift
     module Operations
       module Source
+        STATUS_AVAILABLE, STATUS_UNAVAILABLE = %w[available unavailable].freeze
+
         def self.availability_check(params)
           source_id = params["source_id"]
           raise "Missing source_id for the availability_check request" unless source_id
@@ -13,20 +15,37 @@ module TopologicalInventory
             return if params["timestamp"] < (Time.now.utc - 1.minute)
           end
 
-          puts "Running: availability_check for Source id: #{source_id}"
-
           api_client = SourcesApiClient::DefaultApi.new
 
-          # TODO: Connection check
-
           source = SourcesApiClient::Source.new
-          source.availability_status = "available"
+          source.availability_status = connection_check(api_client, source_id)
 
           begin
             api_client.update_source(source_id, source)
           rescue SourcesApiClient::ApiError => e
             puts "Failed to update Source id:#{source_id} - #{e}"
           end
+        end
+
+        def self.connection_check(api_client, source_id)
+          endpoints = api_client.list_source_endpoints(source_id)&.data || []
+          endpoint = endpoints.find(&:default)
+          return STATUS_UNAVAILABLE unless endpoint
+
+          endpoint_authentications = api_client.list_endpoint_authentications(endpoint.id.to_s).data || []
+          return STATUS_UNAVAILABLE if endpoint_authentications.empty?
+
+          auth_id = endpoint_authentications.first.id
+          auth = Core::AuthenticationRetriever.new(auth_id, nil).process
+          return STATUS_UNAVAILABLE unless auth
+
+          connection_manager = TopologicalInventory::Openshift::Connection.new
+          connection_manager.connect("openshift", :host => endpoint.host, :port => endpoint.port, :token => auth.password)
+
+          STATUS_AVAILABLE
+        rescue SourcesApiClient::ApiError => e
+          puts "Failed to connect to Source id:#{source_id} - #{e}"
+          STATUS_UNAVAILABLE
         end
       end
     end
