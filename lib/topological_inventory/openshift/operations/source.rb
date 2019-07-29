510e3c4b9bc9423b21867d1570be7759fe6854b8
Alberto Bellotti <abellott@redhat.com>
2019-07-29 15:56:47 -0400

    Updated availability_checker to define an identity header based on the passed in external tenant so we can get authorized with the cluster internal API calls made.

--- lib/topological_inventory/openshift/operations/source.rb
+++ lib/topological_inventory/openshift/operations/source.rb
@@ -26,10 +26,8 @@ module TopologicalInventory
             return if params["timestamp"] < (Time.now.utc - 1.minute)
           end
 
-          api_client = SourcesApiClient::DefaultApi.new
-
           source = SourcesApiClient::Source.new
-          source.availability_status = connection_check(api_client, source_id)
+          source.availability_status = connection_check(source_id)
 
           begin
             api_client.update_source(source_id, source)
@@ -40,7 +38,7 @@ module TopologicalInventory
 
         private
 
-        def connection_check(api_client, source_id)
+        def connection_check(source_id)
           endpoints = api_client.list_source_endpoints(source_id)&.data || []
           endpoint = endpoints.find(&:default)
           return STATUS_UNAVAILABLE unless endpoint
@@ -49,7 +47,7 @@ module TopologicalInventory
           return STATUS_UNAVAILABLE if endpoint_authentications.empty?
 
           auth_id = endpoint_authentications.first.id
-          auth = Core::AuthenticationRetriever.new(auth_id, nil).process
+          auth = Core::AuthenticationRetriever.new(auth_id, identity).process
           return STATUS_UNAVAILABLE unless auth
 
           connection_manager = TopologicalInventory::Openshift::Connection.new
@@ -59,6 +57,18 @@ module TopologicalInventory
         rescue => e
           logger.error("Failed to connect to Source id:#{source_id} - #{e.message}")
           STATUS_UNAVAILABLE
+        end
+
+        def identity
+          @identity ||= { "x-rh-identity" => Base64.encode64({ "identity" => { "account_number" => params["external_tenant"] }}.to_json) }
+        end
+
+        def api_client
+          @api_client ||= begin
+            api_client = SourcesApiClient::ApiClient.new
+            api_client.default_headers.merge!(identity)
+            SourcesApiClient::DefaultApi.new(api_client)
+          end
         end
       end
     end
