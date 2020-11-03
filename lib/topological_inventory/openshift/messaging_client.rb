7a7cdefa8b99d25961c43ce80fe1323b5396522c
Patrik Komanek <pkomanek@redhat.com>
2020-11-03 16:41:01 +0100

    Add messaging_client for operations.

---
+++ lib/topological_inventory/openshift/messaging_client.rb
@@ -0,0 +1,39 @@
+require "manageiq-messaging"
+require "topological_inventory/providers/common/messaging_client"
+
+module TopologicalInventory
+  module Openshift
+    class MessagingClient < TopologicalInventory::Providers::Common::MessagingClient
+      OPERATIONS_QUEUE_NAME = "platform.topological-inventory.operations-openshift".freeze
+
+      # Instance of messaging client for Worker
+      def worker_listener
+        @worker_listener ||= ManageIQ::Messaging::Client.open(worker_listener_opts)
+      end
+
+      def worker_listener_queue_opts
+        {
+          :auto_ack    => false,
+          :max_bytes   => 50_000,
+          :service     => OPERATIONS_QUEUE_NAME,
+          :persist_ref => "topological-inventory-operations-openshift"
+        }
+      end
+
+      private
+
+      def worker_listener_opts
+        {
+          :client_ref => default_client_ref,
+          :host       => @queue_host,
+          :port       => @queue_port,
+          :protocol   => :Kafka
+        }
+      end
+
+      def default_client_ref
+        ENV['HOSTNAME'].presence || SecureRandom.hex(4)
+      end
+    end
+  end
+end
